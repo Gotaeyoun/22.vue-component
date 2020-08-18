879325bd858e09c648c9de23f5df85e16dc1885c
Gotaeyoun <ohdin486@gmail.com>
2020-08-18 12:45:41 +0900

    푸쉬

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '@fortawsome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
