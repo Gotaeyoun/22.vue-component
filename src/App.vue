f2bc9481b976c3a8e7a82cbf0a230860174968f5
Gotaeyoun <ohdin486@gmail.com>
2020-08-19 12:38:05 +0900

    연습중

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+	div#app.container
+		nav-bar
+		search-bar(v-on:@submit="onSubmit")
+		product-wrapper(v-show="search.length")
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+import SearchBar from '@/components/SearchBar.vue'
+import ProductWrapper from '@/components/ProductWrapper.vue'
+
+export default {
+	name: 'app',
+	data() {
+		return {
+			search: ''
+		}
+	},
+	components: {
+		'nav-bar': NavBar,
+		'search-bar': SearchBar,
+		'product-wrapper': ProductWrapper,
+	},
+	methods: {
+		onSubmit(value) {
+			this.search = value;
+		}
+	}
+}
+</script>
+
+<style lang="less">
+	
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+	form(v-on:submit.prevent='onSubmit' id='searchForm' name='searchForm')
+		input.form-control(v-model='search' type="text" placeholder="상품 검색어를 입력하세요." autofocus)
+		i.fa.fa-times-circle(v-show='search.length' v-on:click='onReset')
+</template>
+
+<script>
+export default {
+	name: 'search-bar',
+	data() {
+		return {
+			search: ''
+		}
+	},
+	methods: {
+		onSubmit() {
+			// 자식이 부모에게 데이터를 전달하려면 이벤트형식으로 전달해야 한다.
+			this.$emit('@submit', this.search);
+		},
+		onReset() {
+			this.search = '';
+			this.$emit('@submit', this.search);
+		}
+	}
+}
+</script>
+
+<style lang="less">
+	.pointer {cursor: pointer;}
+	#searchForm {
+		position: relative;
+		i {position: absolute; right: 1rem; top: 0.625rem; font-size: 1.25rem; .pointer();}
+	}
+</style>
